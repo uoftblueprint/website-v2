7b76f46bcf9f69f0d53637ed16ca5e61abde45f7
elsiezhu <elsie.zhu2003@gmail.com>
2024-01-19 17:45:13 -0500

    refactor to use css modules instead of inline styling

--- src/chapters-card/ChaptersCard.js
+++ src/chapters-card/ChaptersCard.js
@@ -1,35 +1,25 @@
 import React from "react";
 import { Card, Anchor } from "@mantine/core";
+import "@mantine/core/styles.css";
+import "../chapters-card/ChaptersCard.css";
 
 export function ChaptersCard({ name, description, link }) {
     return (
-        <Card
-            shadow="0px 4px 4px 0px rgba(0, 0, 0, 0.25)"
-            padding="lg"
-            radius="30"
-            style={{ flexShrink: 0 }}
-        >
-            <Card.Section
-                c="#0078E8"
-                style={{ fontSize: "36px", fontWeight: "700", margin: "0" }}
-            >
+        <Card padding="lg">
+            <Card.Section c="#0078E8" className={"chapter-name"}>
                 {name}
             </Card.Section>
 
-            <Card.Section
-                c="#003E77"
-                style={{ fontSize: "24px", fontWeight: "400", margin: "0" }}
-            >
+            <Card.Section c="#003E77" className={"chapter-description"}>
                 {description}
             </Card.Section>
 
-            <Card.Section style={{ margin: "0", paddingTop: "16px" }}>
+            <Card.Section style={{ paddingTop: "16px" }}>
                 <Anchor
                     href={link}
                     target="_blank"
                     c="#0078E8"
                     underline="hover"
-                    style={{ fontSize: "24px", fontWeight: "500" }}
                 >
                     Learn more
                 </Anchor>
