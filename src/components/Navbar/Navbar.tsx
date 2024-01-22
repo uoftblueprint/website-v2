8eb542f913402239e339a3d33fae67565aed568c
shaaaaame <hanxheng@gmail.com>
2024-01-21 22:45:32 -0500

    create navbar component

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Image from "next/image";
+import '@/components/Navbar/Navbar.css';
+import { Button, Group } from '@mantine/core';
+import '@mantine/core/styles.css';
+
+// TODO: change when links are known
+const links = [
+  { link: '/projects', label: 'Projects' },
+  { link: '/team', label: 'Team' },
+  { link: '/chapters', label: 'Chapters' },
+];
+
+const items = links.map((link) => 
+<Button variant="transparent" component="a" href={link.link} color="white" >{link.label}</Button>
+)
+
+function Navbar() {
+  return (
+    <header className='nav'>
+      <div className='nav-logo_container'>
+        <Image src='/logo-with-text.svg' alt='Blueprint Logo' className='nav-logo-img' height={30} width={150}/>
+      </div>
+      <div className='nav-menu_container'>
+        {items}
+        <Button variant="outline" color="white" className="nav-join-btn"> Join our Team!</Button>
+      </div>
+    </header>
+  )
+}
+
+export default Navbar
