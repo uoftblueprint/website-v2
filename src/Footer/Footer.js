574a18f38e09bf4f7ac6ca297f66d005f3dbf139
Jack <junwei.fan@mail.utoronto.ca>
2024-01-25 20:38:56 -0500

    added copyright text + minor refactors

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,44 +1,48 @@
 import React from "react";
-import { Container, Grid, Group, Button, Anchor } from "@mantine/core";
+import {
+  Group,
+  Anchor,
+} from "@mantine/core";
 import "@mantine/core/styles.css";
 import "./Footer.css";
 import Image from "next/image";
 
-export default function Footer() {
+const Footer = () => {
   return (
     <footer className="main-footer">
-      {/* <Container> */}
-        <Group className="footer-menu">
-          <a className="logo" href="/">
-            <Image
-              src="/logo-with-text.svg"
-              alt="Blueprint Logo"
-              width={432}
-              height={83}
-              className="logo"
-            ></Image>
-          </a>
 
-        <Anchor href={"/about"} className="footer-text" underline="hover">
-            {" "}
-            About{" "}
-          </Anchor>
-          <Anchor href={"/projects"} className="footer-text">
-            Projects
-          </Anchor>
-          <Anchor href={"/about"} className="footer-text">
-            {" "}
-            Team{" "}
-          </Anchor>
-          <Anchor href={"/chapters"} className="footer-text">
-            Chapters
-          </Anchor>
-          <Anchor href={"/join"} className="footer-text">
-            Join
-          </Anchor>
-          
-        </Group>
-      {/* </Container> */}
+      <Group className="footer-menu">
+        <a className="logo" href="/">
+          <Image
+            src="/logo-with-text.svg"
+            alt="Blueprint Logo"
+            width={432}
+            height={83}
+            className="logo"
+          ></Image>
+        </a>
+
+        <Anchor href={"/about"} className="footer-text">
+          About
+        </Anchor>
+        <Anchor href={"/projects"} className="footer-text">
+          Projects
+        </Anchor>
+        <Anchor href={"/team"} className="footer-text">
+          Team
+        </Anchor>
+        <Anchor href={"/chapters"} className="footer-text">
+          Chapters
+        </Anchor>
+        <Anchor href={"/join"} className="footer-text">
+          Join
+        </Anchor>
+      </Group>
+
+      <h5 className="copyright-text"> &copy; Blueprint 2023</h5>
+
     </footer>
   );
-}
+};
+
+export default Footer;
