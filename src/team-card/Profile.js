d242ce7b9f3f0ac20fd156ab872ea1839ce93641
Michelle <michh2005@gmail.com>
2024-01-29 15:20:16 -0500

    style profile text and picture

--- src/team-card/Profile.js
+++ src/team-card/Profile.js
@@ -3,13 +3,15 @@
 import React from "react";
 import { Avatar, Text } from "@mantine/core";
 import '@mantine/core/styles.css';
+import classes from "./Profile.css"
+
 
 function Profile({name, role, profilePicture}) {
     return (
-        <div>
-            <Avatar src={profilePicture}/>
-            <Text>{name}</Text>  
-            <Text>{role}</Text>
+        <div className={classes.profile}>
+            <Avatar size="128px" src={profilePicture}/>
+            <Text pt="md" size="18px" ta="center" c="#000">{name}</Text>  
+            <Text pt="5px" size="16px" ta="center" c="#C4C4C4">{role}</Text>
         </div>
     )
 }
