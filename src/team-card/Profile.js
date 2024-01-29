e7bd91d18339031b09995b4bfb6fbfe731dc703f
Michelle <michh2005@gmail.com>
2024-01-29 18:10:07 -0500

    test Center mantine component using with column flex

--- src/team-card/Profile.js
+++ src/team-card/Profile.js
@@ -1,20 +1,20 @@
 'use client'
 
 import React from "react";
-import { Avatar, Text, MantineProvider } from "@mantine/core";
+import { Center, Avatar, Text } from "@mantine/core";
 import '@mantine/core/styles.css';
 import classes from "./Profile.css"
 
 
 function Profile({name, role, profilePicture}) {
     return (
-        <MantineProvider>
-            <div className={classes.profile}>
-                <Avatar size="128px" src={profilePicture}/>
-                <Text pt="md" size="18px" ta="center" c="#000">{name}</Text>  
-                <Text pt="5px" size="16px" ta="center" c="#C4C4C4">{role}</Text>
-            </div>
-        </MantineProvider>
+        <Center>
+        {/* <div className={classes.profile}> */}
+            <Avatar size="128px" src={profilePicture}/>
+            <Text pt="md" size="18px" ta="center" c="#000">{name}</Text>  
+            <Text pt="5px" size="16px" ta="center" c="#C4C4C4">{role}</Text>
+        {/* </div> */}
+        </Center>
     )
 }
 
