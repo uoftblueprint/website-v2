f3f4d8aae3182931178bb1e661d785b4bd43d8dd
Michelle <michh2005@gmail.com>
2024-01-29 17:59:56 -0500

    styled title and columns&spacing of team grid

--- src/team-card/TeamCard.js
+++ src/team-card/TeamCard.js
@@ -2,15 +2,21 @@
 
 import React from "react";
 import Profile from "./Profile";
-import { MantineProvider, Title, SimpleGrid } from "@mantine/core";
+import { createTheme, MantineProvider, Title, SimpleGrid } from "@mantine/core";
 import '@mantine/core/styles.layer.css';
+import "./TeamCard.module.css"
+
+const theme = createTheme({
+    fontFamily: 'Poppins, sans-serif',
+  });
 
 function TeamCard({title, team_members}) {
+
     return (
         <MantineProvider theme={theme}>
-            <Title>{title}</Title>
+            <Title className="teamName" ta="center" pb="xl" c="#0078E8">{title}</Title>
 
-            <SimpleGrid>
+            <SimpleGrid style={{alignItems:"start",}} spacing="lg" cols={{lg:6, sm:4, xs:2}}>
                 {team_members.map( (team_member, index) => 
                             <Profile 
                             key={index}
