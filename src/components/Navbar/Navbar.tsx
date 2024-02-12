d2c321749619933867493a9c7b6b4fb0bb355fcc
shaaaaame <hanxheng@gmail.com>
2024-02-12 17:19:10 -0500

    completed responsive navbar - appshell nav - added side menu component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
-import { Button } from '@mantine/core';
+import { Burger, Button, Container, Transition } from '@mantine/core';
 import '@/components/Navbar/Navbar.css';
+import '@/components/Navbar/SideNavMenu.css';
 import '@mantine/core/styles.css';
 
 const links = [
@@ -14,16 +15,30 @@ const items = links.map((link) =>
   <Button variant="transparent" component="a" href={link.link} color="white" classNames={{label: "nav-link"}}>{link.label}</Button>
 )
 
-function Navbar() {
+items.push(
+  <Button variant="filled" color="#FFFFFF" className="nav-join-btn">Join our Team!</Button>
+)
+
+function Navbar({opened, toggleOpened}) {
+
   return (
     <header className='nav'>
       <a className='nav-logo_tag' href="/">
         <Image src='/logo-with-text.svg' alt='Blueprint Logo' height={30} width={150}/>
       </a>
       <div className='nav-menu_container'>
-        {items}
-        <Button variant="outline" color="white" className="nav-join-btn">Join our Team!</Button>
+        <Container visibleFrom="sm">
+          {items}
+        </Container>
+        <Burger
+          opened={opened}
+          onClick={toggleOpened}
+          hiddenFrom="sm"
+          size="md"
+          color="#FFFFFF"
+        />
       </div>
+      
     </header>
   )
 }
