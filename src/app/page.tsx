596c7fe108e9d288f11e5048aaa6f36e50f028df
Gabriel Vainer <67750020+vai9er@users.noreply.github.com>
2024-02-18 17:46:18 -0500

    update page.tsx template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,5 @@
 import { ChaptersCard } from "@/chapters-card/ChaptersCard";
 
 export default function Home() {
-  return (
-    <div>
-      {
-        <ChaptersCard
-          name="gaaaab"
-          description="bennnnn"
-          link="https://google.com"
-        />
-      }
-    </div>
-  );
+  return <div>{/* add components here! */}</div>;
 }
