4cfbc6a8a6c9fd7a607ec4fbafd582c3349ea589
Gabriel Vainer <67750020+vai9er@users.noreply.github.com>
2024-03-02 14:00:56 -0500

    update component properties

--- src/projects-card/ProjectsCard.js
+++ src/projects-card/ProjectsCard.js
@@ -2,25 +2,20 @@ import { Card, CardSection, Image } from "@mantine/core";
 import "@mantine/core/styles.css";
 import "../projects-card/ProjectsCard.css";
 
-export function ProjectsCard({name, description, image}) {
-    return (
-        <Card padding="lg">
-            <CardSection>
-                <Image
-                    src={image}
-                    radius="md"
-                    alt= {name}
-                    className={"image"}
-                />
-            </CardSection>
+export function ProjectsCard({ name, description, image }) {
+  return (
+    <Card padding="lg">
+      <CardSection>
+        <Image src={image} radius="md" alt={name} className={"image"} />
+      </CardSection>
 
-            <CardSection c="#0078E8" className={"project-name"}>
-                {name}
-            </CardSection>
+      <CardSection c="#0078E8" className={"project-name"}>
+        {name}
+      </CardSection>
 
-            <CardSection c="#0078E8" className={"project-description"}>
-                {description}
-            </CardSection>
-        </Card>
-    )
+      <CardSection c="#0078E8" className={"project-description"}>
+        {description}
+      </CardSection>
+    </Card>
+  );
 }
