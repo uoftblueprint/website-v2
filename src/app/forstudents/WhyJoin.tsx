3e7ee5190155c1efd201051308df94ae5ea1e128
shaaaaame <hanxheng@gmail.com>
2024-03-03 17:36:53 -0500

    minor responsive tweaks - font size, width

--- src/app/forstudents/WhyJoin.tsx
+++ src/app/forstudents/WhyJoin.tsx
@@ -8,18 +8,24 @@ function WhyJoin({ img }) {
       align="center"
       justify="space-between"
       className="whyjoin"
-      direction={{ sm: "row", base: "column" }}
+      direction={{ md: "row", base: "column" }}
+      columnGap={"30px"}
     >
-      <Container w={{ base: "100%" }} h={{ sm: "100%", base: "auto" }} p={0}>
+      <Container w={{ base: "100%" }} h={{ md: "100%", base: "auto" }} p={0}>
         <Image src={img} alt="why join img" />
       </Container>
-      <div className="whyjoin-text">
+      <Container
+        className="whyjoin-text"
+        w={{ md: "100%", base: "100%" }}
+        h={{ md: "100%", base: "auto" }}
+        p={0}
+      >
         <h2>Why join?</h2>
         <p>
           Interested in making a change in the community around you? Want to
           gain experience working in a software development team? Join us!
         </p>
-      </div>
+      </Container>
     </Flex>
   );
 }
