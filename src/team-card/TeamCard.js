74b23769af5d7eeb6d4f30019b6f2de4b577e188
Michelle <michh2005@gmail.com>
2024-03-03 15:16:48 -0500

    refactor: add maxwidth to names, remove mantineProvider, change column width

--- src/team-card/TeamCard.js
+++ src/team-card/TeamCard.js
@@ -2,19 +2,13 @@
 
 import Profile from "./Profile";
 import { Title, SimpleGrid, Container } from "@mantine/core";
-import { useMediaQuery } from "@mantine/hooks";
 import "@mantine/core/styles.css";
 import "./TeamCard.css";
 
 function TeamCard({ title, team_members }) {
-  const small = useMediaQuery("(max-width: 500px)");
-  const titleSize = small ? "20px" : "30px";
-
   return (
     <Container size="80%">
-      <Title size={titleSize} c="#0078E8">
-        {title}
-      </Title>
+      <Title c="#0078E8">{title}</Title>
 
       <SimpleGrid spacing="lg" cols={{ lg: 6, md: 4, sm: 3, xs: 2, base: 2 }}>
         {team_members.map((team_member, index) => (
