326a0808e78ef213e52a7263803a3a76b82e1924
Michelle <michh2005@gmail.com>
2024-03-06 19:26:36 -0500

    chore: add content

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,15 @@
 "use client";
 import React from "react";
-import {Container, Title, Text, Image} from "@mantine/core"
+import { Container, Title, Text, Image } from "@mantine/core"
 import _ from "@/config/firebase";
 
 export default function Home() {
     return (
       // Top Section of Page
         <Container>
-          <Title></Title>
-          <Text></Text>
-          <Image></Image>
+          <Title>Hi! 👋  We’re Blueprint.</Title>
+          <Text>We’re a group of passionate students at the University of Toronto dedicated to building software to give back to our community!</Text>
+          <Image alt="Group of People" src="team at work_solid I 1.png"></Image>
         </Container>
     );
 }
