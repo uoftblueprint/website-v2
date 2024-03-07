14287d7d8bdfc96de50fa739b3811d165331760c
Michelle <michh2005@gmail.com>
2024-03-06 19:32:03 -0500

    chore: move code from page.js to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
 "use client";
+import { Container, Title, Text, Image } from "@mantine/core"
 
 export default function Home() {
-  return <div>{/* add components here! */}</div>;
+  return (
+    // Top Section of Page
+    <Container>
+    <Title>Hi! 👋  We’re Blueprint.</Title>
+    <Text>We’re a group of passionate students at the University of Toronto dedicated to building software to give back to our community!</Text>
+    <Image alt="Group of People" src="team at work_solid I 1.png"></Image>
+  </Container>
+  );
 }
