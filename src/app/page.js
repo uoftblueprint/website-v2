1141ff6c64ab581caed4657f78bfc41fa09a22e0
Michelle <michh2005@gmail.com>
2024-03-06 19:11:31 -0500

    chore: add mantine imports and structure of top section

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,15 @@
 "use client";
 import React from "react";
+import {Container, Title, Text, Image} from "@mantine/core"
 import _ from "@/config/firebase";
 
 export default function Home() {
     return (
-        
+      // Top Section of Page
+        <Container>
+          <Title></Title>
+          <Text></Text>
+          <Image></Image>
+        </Container>
     );
 }
