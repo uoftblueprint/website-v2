845c73ecba8ce81c363edc9eb231d8cad0aabf94
Michelle <michh2005@gmail.com>
2024-03-10 17:11:57 -0400

    refactor: move top section code from home page to component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,6 @@
 "use client";
-import { Container, Title, Text, Image } from "@mantine/core"
 
 export default function Home() {
   return (
-    // Top Section of Page
-    <Container>
-    <Title>Hi! 👋  We’re Blueprint.</Title>
-    <Text>We’re a group of passionate students at the University of Toronto dedicated to building software to give back to our community!</Text>
-    <Image alt="Group of People" src="team at work_solid I 1.png"></Image>
-  </Container>
   );
 }
