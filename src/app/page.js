12a2435db526d7ffb22766fb905a3ce36371ccd6
shaaaaame <hanxheng@gmail.com>
2024-03-11 23:51:58 -0400

    Added drop shadow

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,13 @@
-import Image from 'next/image';
-import _ from '@/config/firebase';
-import styles from '@/app/page.module.css';
-import TestPage from '@/app/TestPage.js';
-import { MantineProvider } from '@mantine/core';
+import Image from "next/image";
+import _ from "@/config/firebase";
+import styles from "@/app/page.module.css";
+import { MantineProvider } from "@mantine/core";
+import Banner from "@/components/Banner/Banner";
 
 export default function Home() {
     return (
         <MantineProvider>
-            <TestPage />
-
+            <Banner title="a" text="b" button={{ text: "a", link: "a" }} />
         </MantineProvider>
     );
 }
