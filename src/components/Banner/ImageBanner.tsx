abfe0261fedab7553ae3388bf1d7da8577fb67f7
shaaaaame <hanxheng@gmail.com>
2024-03-12 00:23:35 -0400

    minor refactoring, remove unused imports

--- src/components/Banner/ImageBanner.tsx
+++ src/components/Banner/ImageBanner.tsx
@@ -1,17 +1,16 @@
-import { Container, Flex, Button } from "@mantine/core";
+import { Container } from "@mantine/core";
 import "@/components/Banner/Banner.css";
-import React from "react";
 
 function ImageBanner({ src }) {
-	return (
-		<Container
-			fluid
-			className="non-hero banner-container image-banner"
-			style={{
-				backgroundImage: `linear-gradient(to bottom, #0078e8, rgba(0, 0, 0, 0)), url(${src})`,
-			}}
-		></Container>
-	);
+  return (
+    <Container
+      fluid
+      className="non-hero banner-container image-banner"
+      style={{
+        backgroundImage: `linear-gradient(to bottom, #0078e8, rgba(0, 0, 0, 0)), url(${src})`,
+      }}
+    ></Container>
+  );
 }
 
 export default ImageBanner;
