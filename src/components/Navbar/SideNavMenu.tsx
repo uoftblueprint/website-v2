638aa1babd291d7dd66d91b76652a456463d408d
Han Xheng Chew <81433191+shaaaaame@users.noreply.github.com>
2024-03-14 23:45:51 -0400

    INT-11: Fix navbar responsiveness (#8)
    * implemented footer text and logo
    
    * first attempt at project component
    
    * added copyright text + minor refactors
    
    * create team card js and css files
    
    * create profile component to be
    used in team card
    
    * created unstyled profile component
    
    * style profile text and picture
    
    * import poppins font for profile text
    
    * test Profile functionality and create test_team
    
    * add profile pic test img
    
    * moved cat profile pic test img
    
    * create unstyled teamCard skeleton
    
    * styled title and columns&spacing of team grid
    
    * fix TeamCard styles import naming
    
    * test Center mantine component using with column flex
    
    * refactor to set text+title styling w/css module
    
    * refactor Profile styles import and removed unnecessary style classes
    
    * remove unnecessary use client declarations
    
    * refactor inline styling of Center component in Profile component
    
    * add linkedin page link to profile pictures
    
    * refactor linkedin anchor and test mantine breakpoints
    
    * Module not found bug - troubleshooting
    
    * complete projects card
    
    * remove kermit picture
    
    * setup
    
    * remove temp theme object
    
    * responsive styling for avatar sizes
    
    * change theme breakpoints, only edit xs
    
    * refactor: team name sizing
    
    * refactor: text-align for profile to css file
    
    * refactor: responsive sizing for title using query
    
    * testing team card on empty page
    
    * update styling
    
    * completed responsive navbar
    - appshell nav
    - added side menu component
    
    * minor refactoring + header height change
    
    * minor refactoring + changed nav height according to figma
    
    * updated to match updated nav on figma
    
    * added background dim
    
    * refactor: try using center as container for teamcard
    
    * refactor profile component to use id for text
    
    * minor changes to page.js
    
    * Installed TS and dependencies
    
    * configured Prettier and ESLint/ made workflow yml
    
    * configured Prettier and ESLint/ made workflow yml
    
    * Fixed ESLint Issues
    
    * fixed next config
    
    * chore: using test data
    
    * re-adjust image borders and added media queries
    
    * from main: updates to package-lock and package
    
    * update page.tsx template
    
    * Update page.tsx
    
    * refactor: remove unused imports
    
    * minor refactors
    
    * made footer responsive to smaller screens
    
    * deleted files
    
    * integrated font
    
    * integrated font
    
    * added navbar and footer to root layout
    
    * Added logos
    
    * finished adding images
    
    * adjusted units to vw to make it responsive
    
    * changed units to vw to make footer responsive
    
    * font changes
    
    * deleting footer files
    
    * refactor: remove redundant styles
    
    * refactor: add responsive text sizing to profile
    
    * refactor: remove static font-size
    
    * refactor: replace enclosing Center with Container
    
    * refactor: remove redundant styles
    
    * edited breakpoint for side menu
    
    * removed navbar component and changed height
    
    * update format of page.tsx
    
    * setting text box margin to 0
    
    * refactor: adding pull request changes
    
    * refactor: add maxwidth to names, remove mantineProvider, change column width
    
    * completed responsive navbar
    - appshell nav
    - added side menu component
    
    * minor refactoring + header height change
    
    * minor refactoring + changed nav height according to figma
    
    * updated to match updated nav on figma
    
    * added background dim
    
    * edited breakpoint for side menu
    
    * minor refactoring
    
    * minor refactoring
    
    * minor refactoring
    
    * removed unused imports
    
    ---------
    
    Co-authored-by: Jack <junwei.fan@mail.utoronto.ca>
    Co-authored-by: Sarah Wang <sarahxp.wang@mail.utoronto.ca>
    Co-authored-by: Michelle <michh2005@gmail.com>
    Co-authored-by: Gabriel Vainer <67750020+vai9er@users.noreply.github.com>

---
+++ src/components/Navbar/SideNavMenu.tsx
@@ -0,0 +1,30 @@
+import { Button } from "@mantine/core";
+import "@/components/Navbar/SideNavMenu.css";
+
+function SideNavMenu({ links }) {
+  const items = links.map((link) => (
+    <Button
+      variant="transparent"
+      component="a"
+      href={link.link}
+      color="white"
+      className="nav-link side-nav-link"
+      key={link.label}
+    >
+      {link.label}
+    </Button>
+  ));
+
+  items.push(
+    <Button variant="filled" color="white" className="nav-join-btn" key="join">
+      Join our Team!
+    </Button>,
+  );
+  return (
+    <div className="side-nav">
+      <div className="side-menu">{items}</div>
+    </div>
+  );
+}
+
+export default SideNavMenu;
