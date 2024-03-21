74aa38d52744a338907bef0d8996a7cad727d64b
Gabriel Vainer <gabe.vainer@gmail.com>
2024-03-20 21:47:21 -0400

    fixes for RoleCard

--- RoleCard/RoleCard.tsx
+++ RoleCard/RoleCard.tsx
@@ -1,27 +1,34 @@
-import React from 'react';
-import { Card, Image, Text, Button, Center, Anchor } from '@mantine/core';
-import "./RoleCard.css"
+import React from "react";
+import { Card, Image, Text, Button, Center, Anchor } from "@mantine/core";
+import "./RoleCard.css";
 
-export default function RoleCard({ role, link}){
+export default function RoleCard({ role, link }) {
   return (
-      // <Card className='roleCard' shadow="lg" radius="xl" p="xl" style={{ width: 'calc(33.333% - 20px)', marginBottom: '20px' }}>
-      <Card className='roleCard' shadow="lg" radius="xl" p="xl">
-        <Center>
-          <Image src="/monitor.svg" />
-        </Center>
-        <Center my="xs">
-          <Text c="#0078E8" fw={500} className='roleTitle'>
-            {role}
-          </Text>
-        </Center>
-        <Center>
-          <Anchor href={link} target="_blank" rel="noopener noreferrer">
-          <Button rightSection={<Image className='chevron' src="/chevron-right.svg"/>} variant="filled" radius="xl" size="xl" className="buttonWithImage">
-            Apply
+    // <Card className='roleCard' shadow="lg" radius="xl" p="xl" style={{ width: 'calc(33.333% - 20px)', marginBottom: '20px' }}>
+    <Card className="roleCard" shadow="lg" radius="xl" p="xl">
+      <Center>
+        <Image src="/monitor.svg" />
+      </Center>
+      <Center my="xs">
+        <Text c="#0078E8" fw={500} className="roleTitle">
+          {role}
+        </Text>
+      </Center>
+      <Center>
+        <Anchor href={link} target="_blank" rel="noopener noreferrer">
+          <Button
+            rightSection={
+              <Image className="chevron" src="/chevron-right.svg" />
+            }
+            variant="filled"
+            radius="xl"
+            size="xl"
+            className="buttonWithImage"
+          >
+            Apply Now
           </Button>
-          </Anchor>
-        </Center>
-      </Card>
+        </Anchor>
+      </Center>
+    </Card>
   );
-};
-
+}
