8e49475e742ebb12443634cc543f8daa7adc3b54
Michelle <michh2005@gmail.com>
2024-03-22 16:45:37 -0400

    refactor: accidentally deleted code, put it back on

--- src/for-students-FAQ/for-students-FAQ.js
+++ src/for-students-FAQ/for-students-FAQ.js
@@ -1,5 +1,37 @@
-"use client";
+import { Accordion, Container, Title, Divider, Image } from "@mantine/core";
+import "./for-students-FAQ.css";
 
-export default function Home() {
-  return <div>{/* add components here! */}</div>;
+function FaqCard({ faq_info }) {
+  const items = faq_info.map((faq, index) => (
+    <Accordion.Item className="accordion-item" key={index} value={faq.question}>
+      <Accordion.Control className="question">
+        <b>{faq.question}</b>
+      </Accordion.Control>
+
+      <Accordion.Panel>{faq.answer}</Accordion.Panel>
+    </Accordion.Item>
+  ));
+
+  return (
+    <Container className="faq-container" fluid>
+      <Title className="faq-title">FAQ</Title>
+
+      <Divider className="divider" my="lg"></Divider>
+
+      <p>
+        Here are some frequently asked questions that we’ve compiled! If you
+        have further questions, please contact us on our Instagram or
+        contact@uoftblueprint.org.
+      </p>
+
+      <Accordion
+        variant="separated"
+        chevron={<Image src="chevron-down.png" />}
+      >
+        {items}
+      </Accordion>
+    </Container>
+  );
 }
+
+export default FaqCard;
