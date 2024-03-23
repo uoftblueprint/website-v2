0a510b9a4aed2028d25a04c9bf965ae77ceec430
Michelle <michh2005@gmail.com>
2024-03-23 18:25:57 -0400

    chore: added topsection css file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
 "use client";
 import TopSection from "./home-page-top-section/TopSection.js";
-import Bottomsection from "./home-page-top-section/BottomSection.js"
+import Bottomsection from "./home-page-top-section/BottomSection.js";
 import HeroBanner from "@/components/Banner/HeroBanner.jsx";
-
 
 export default function Home() {
   return (
     <div>
       <HeroBanner></HeroBanner>
-      <TopSection/>
-      <Bottomsection/>
+      <TopSection />
+      <Bottomsection />
     </div>
   );
 }
