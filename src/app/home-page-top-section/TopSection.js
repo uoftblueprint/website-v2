48957e932530e45c0f5ad68ae7313e37ac003ff9
Michelle <michh2005@gmail.com>
2024-03-23 23:05:06 -0400

    refactor: modify padding for topsection and width styles

--- src/app/home-page-top-section/TopSection.js
+++ src/app/home-page-top-section/TopSection.js
@@ -4,15 +4,18 @@ import styles from "./TopSection.module.css";
 export default function TopSection() {
   // Top Section of Page
   // Figure out how to center on home page
+  {
+    console.log(Text.classes);
+  }
   return (
     <Container className={styles.container} size="90%">
       <Title className={styles.title}>Hi! 👋 We’re Blueprint.</Title>
-      <Text c="#0078e8" className={styles.text}>
+      <Text className={styles.text}>
         We’re a group of passionate students at the University of Toronto
         dedicated to building software to give back to our community!
       </Text>
       <Image
-        className="image"
+        className={styles.image}
         alt="Group of People"
         src="/people_images/teamwork.svg"
       ></Image>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,21 @@
 import TopSection from "./home-page-top-section/TopSection.js";
 import Bottomsection from "./home-page-top-section/BottomSection.js";
 import HeroBanner from "@/components/Banner/HeroBanner.tsx";
+import { ProjectsCard } from "src/projects-card/ProjectsCard.js";
+import { Text } from "@mantine/core";
+
+let team_data = [
+  { name: "adsf", role: "asdf", profilePicture: "" },
+  { name: "adsf", role: "asdf", profilePicture: "" },
+  { name: "adsf", role: "asdf", profilePicture: "" },
+  { name: "adsf", role: "asdf", profilePicture: "" },
+];
 
 export default function Home() {
   return (
     <div>
-      {/* <HeroBanner></HeroBanner> */}
+      {/* <ProjectsCard name="asdf" description="asdf" image=""></ProjectsCard> */}
+      <HeroBanner></HeroBanner>
       <TopSection />
       <Bottomsection />
     </div>
