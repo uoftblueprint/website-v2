4705d06cb9fd16f437ad5ff52c64ea654d9ddc95
Michelle <michh2005@gmail.com>
2024-03-27 22:04:29 -0400

    chore: put page.tsx content back on

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,21 @@
 "use client";
+import TopSection from "./home-page-top-section/TopSection.js";
+import Bottomsection from "./home-page-top-section/BottomSection.js";
+import HeroBanner from "@/components/Banner/HeroBanner.tsx";
+import Footer from "src/components/Footer/Footer.js";
+import { ChaptersCard } from "src/chapters-card/ChaptersCard.tsx";
+import { Text } from "@mantine/core";
+
 
 export default function Home() {
-  return <div>{/* add components here! */}</div>;
+  return (
+    <div>
+      {/* <ProjectsCard name="asdf" description="asdf" image=""></ProjectsCard> */}
+      <HeroBanner></HeroBanner>
+      <TopSection />
+      <Bottomsection />
+      {/* <ChaptersCard name="adsf" description="asdf" link=""></ChaptersCard> */}
+      {/* <Footer></Footer> */}
+    </div>
+  );
 }
