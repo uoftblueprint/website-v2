b42f40953f094257e76d40d388d99cd519e08854
Michelle <michh2005@gmail.com>
2024-03-30 13:39:06 -0400

    chore: added all requried files

---
+++ src/app/white-section/white-section.js
@@ -0,0 +1,6 @@
+import { Container } from "@mantine/core";
+import "./BottomSection.css";
+
+export default function BottomSection() {
+  return <Container size="100%" className="bottom-section"></Container>;
+}
