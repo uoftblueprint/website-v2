8ca16192219bcc2a72f0bf1bf1dec51178444e23
Michelle <michh2005@gmail.com>
2024-03-30 13:35:48 -0400

    delete folder

--- src/app/about-us-components/white-section.js
+++
@@ -1,9 +0,0 @@
-import "./white-section.css";
-import { Container } from "@mantine/core";
-
-
-export default function BottomSection() {
-    return (
-      <Container size="100%" className="bottom-section"></Container>
-    )
-}
