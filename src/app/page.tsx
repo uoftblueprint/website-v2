5db7b7ee3b152dbef3b36dd2e8ce804a61cf2038
Michelle <michh2005@gmail.com>
2024-03-31 23:29:40 -0400

    refactor: remove unused imports in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,18 @@
 import TopSection from "./home-page-top-section/TopSection.js";
 import Bottomsection from "./home-page-top-section/BottomSection.js";
 import ProjectCardsGrid from "./home-page-top-section/ProjectCardsGrid.js";
-import ProjectsCard from "@/projects-card/ProjectsCard.js";
 import HeroBanner from "@/components/Banner/HeroBanner.tsx";
-import { SimpleGrid } from "@mantine/core";
 
 let cards_info = [
   {
     name: "The Period Purse",
+    description:
+      "A Menstrual Tracking application for iOS and Android, built by youth for youth.",
+    image: "/the-period-purse.jpeg",
+    link: "https://www.google.com/",
+  },
+  {
+    name: "Home for Heroes",
     description:
       "A Menstrual Tracking application for iOS and Android, built by youth for youth.",
     image: "/the-period-purse.jpeg",
@@ -26,57 +31,10 @@ let cards_info = [
 export default function Home() {
   return (
     <>
-      <HeroBanner></HeroBanner>
+      <HeroBanner />
       <TopSection />
       <Bottomsection />
       <ProjectCardsGrid cards_info={cards_info}></ProjectCardsGrid>
-      {/* <SimpleGrid
-        cols={2}
-        spacing={"xl"}
-        verticalSpacing={{ base: "md", sm: "xl" }}
-      >
-        <ProjectsCard
-          name="The Period Purse"
-          description="A Menstrual Tracking application for iOS and Android, built by youth for youth."
-          image={"/the-period-purse.jpeg"}
-          hasOverlay={true}
-          years={["2021-2022", "2023"]}
-          projectDescription="Description"
-          nonprofitDescription="Description"
-          team={[
-            { name: "NAME", role: "ROLE", imageSrc: "" },
-            { name: "NAME1", role: "ROLE", imageSrc: "" },
-            { name: "NAME2", role: "ROLE", imageSrc: "" },
-            { name: "NAME3", role: "ROLE", imageSrc: "" },
-            { name: "NAME4", role: "ROLE", imageSrc: "" },
-            { name: "NAME5", role: "ROLE", imageSrc: "" },
-            { name: "NAME6", role: "ROLE", imageSrc: "" },
-          ]}
-          webLink=""
-          githubLink=""
-        />
-        <ProjectsCard
-          name="The Period Purse"
-          description="A Menstrual Tracking application for iOS and Android, built by youth for youth."
-          image={"/the-period-purse.jpeg"}
-          hasOverlay={true}
-          years={["2021-2022", "2023"]}
-          projectDescription="Description"
-          nonprofitDescription="Description"
-          team={[
-            { name: "NAME", role: "ROLE", imageSrc: "" },
-            { name: "NAME1", role: "ROLE", imageSrc: "" },
-            { name: "NAME2", role: "ROLE", imageSrc: "" },
-            { name: "NAME3", role: "ROLE", imageSrc: "" },
-            { name: "NAME4", role: "ROLE", imageSrc: "" },
-            { name: "NAME5", role: "ROLE", imageSrc: "" },
-            { name: "NAME6", role: "ROLE", imageSrc: "" },
-          ]}
-          webLink=""
-          githubLink=""
-        />
-      </SimpleGrid>
-      <ProjectCardsGrid cards_info={cards_info} /> */}
     </>
   );
 }
