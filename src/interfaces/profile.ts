ccef9f249095e922e09008a6a59a943ed807271c
leowrites <leo@liuwork.ca>
2024-04-26 17:28:57 -0700

    add profile interface

---
+++ src/interfaces/profile.ts
@@ -0,0 +1,8 @@
+export interface Profile {
+    linkedinLink: string;       // URL to LinkedIn profile
+    name: string;               // Full name of team member
+    profileImagePath: string;   // Path to profile picture in cloud storage
+    profileImage: string;       // Url to profile picture
+    role: string;               // Role within the team
+    team: string;               // Team name
+}
