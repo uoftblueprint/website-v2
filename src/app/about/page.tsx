0326acd1f76fe7655d1daeed7206766bababeaad
Lily Phan <79292581+pH-li@users.noreply.github.com>
2024-10-30 10:08:19 -0400

    Incorporated white-section.css into white-section.tsx in the about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import WhiteSection from "./white-section/white-section.tsx";
+import WhiteSection from "./white-section/white-section.js";
 import ImageBanner from "@/components/Banner/ImageBanner.tsx";
 
 export default function About() {
