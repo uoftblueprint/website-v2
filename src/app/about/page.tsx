e605d1f093d03d1a220a321d42ff5646621feb3a
Lily Phan <79292581+pH-li@users.noreply.github.com>
2024-10-30 10:08:19 -0400

    Incorporated about.css into page.tsx in the about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,10 @@
 "use client";
 import WhiteSection from "./white-section/white-section.js";
-import "./about.css";
 import ImageBanner from "@/components/Banner/ImageBanner.tsx";
 
 export default function About() {
   return (
-    <div>
+    <div style={{backgroundColor: "#e7f3ff"}}>
       <ImageBanner src={"/about_page/blueprint-family-photo.png"} />
       <WhiteSection />
     </div>
