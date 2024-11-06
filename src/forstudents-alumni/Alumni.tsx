90281b6c7768ff1a79e26fb3a4fc6781f3c32ee8
alexlewis9 <150391000+alexlewis9@users.noreply.github.com>
2024-11-06 18:40:38 -0500

    remove css alumni, roles

--- src/forstudents-alumni/Alumni.tsx
+++ src/forstudents-alumni/Alumni.tsx
@@ -1,12 +1,20 @@
-import { Title } from "@mantine/core";
-import "./Alumni.css";
+import { Center, Title } from "@mantine/core";
 
 function Alumni() {
   return (
-    <div className="alumni">
-      <Title className="alumni-title">
-        Our Alumni have gone on to innovate at:
-      </Title>
+    <div
+      className="alumni"
+      style={{
+        borderRadius: "0px 0px 300px 300px",
+        backgroundColor: "#e7f3ff",
+        color: "#0078e8",
+      }}
+    >
+      <Center>
+        <Title className="alumni-title" size="3vw" style={{ padding: "5%" }}>
+          Our Alumni have gone on to innovate at:
+        </Title>
+      </Center>
     </div>
   );
 }
