5715d767912bc5b83a482d6cb29d63f73e2f638b
alexlewis9 <150391000+alexlewis9@users.noreply.github.com>
2024-11-06 18:40:38 -0500

    remove css alumni, roles

--- src/forstudents-roles/Roles.tsx
+++ src/forstudents-roles/Roles.tsx
@@ -1,7 +1,6 @@
 "use client";
-import "@/forstudents-roles/Roles.css";
 import RoleCard from "../RoleCard/RoleCard";
-import { Divider, Grid, Title } from "@mantine/core";
+import { Center, Divider, Grid, Title } from "@mantine/core";
 
 const info = [
   { role: "Software Developer", link: undefined },
@@ -23,9 +22,22 @@ const row = info.map((info) => (
 export default function Roles() {
   return (
     <div>
-      <Title className="open-roles-title">Open Roles</Title>
+      <Center>
+        <Title
+          className="open-roles-title"
+          size="4vw"
+          style={{ color: "#0078e8", padding: "4vw 0vw 1vw 0vw" }}
+        >
+          Open Roles
+        </Title>
+      </Center>
       <Divider w="30%" color="#e7f3ff" size={"1px"} mx="auto" />
-      <Grid justify="space-between" align={"center"} className={"grid"}>
+      <Grid
+        justify="space-between"
+        align={"center"}
+        className={"grid"}
+        style={{ padding: "1vw 4vw 4vw 4vw" }}
+      >
         {row}
       </Grid>
     </div>
