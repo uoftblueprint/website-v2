1d0931acfa3a6deb0e7098166be7673d9eb94168
alexlewis9 <150391000+alexlewis9@users.noreply.github.com>
2024-11-09 19:10:50 -0500

    fix role cards text and icons to not get too small

--- src/RoleCard/RoleCard.tsx
+++ src/RoleCard/RoleCard.tsx
@@ -6,7 +6,7 @@ export default function RoleCard({ role, link }) {
     // <Card className='roleCard' shadow="lg" radius="xl" p="xl" style={{ width: 'calc(33.333% - 20px)', marginBottom: '20px' }}>
     <Card className="roleCard" shadow="lg" radius="xl" p="xl">
       <Center>
-        <Image className="moniter" src="/monitor.svg" w="4vw" h="4vw" />
+        <Image className="moniter" src="/monitor.svg" />
       </Center>
       <Center my="xs">
         <Text c="#0078E8" fw={500} className="roleTitle">
@@ -15,14 +15,7 @@ export default function RoleCard({ role, link }) {
       </Center>
       <Center>
         <Button
-          rightSection={
-            <Image
-              className="chevron"
-              src="/chevron-right.svg"
-              w="1.5vw"
-              h="1.5vw"
-            />
-          }
+          rightSection={<Image className="chevron" src="/chevron-right.svg" />}
           variant="filled"
           radius="xl"
           size="xl"
