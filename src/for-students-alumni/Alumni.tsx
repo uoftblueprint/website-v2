23ccff7d9bc38264c7bea7ab4f17709c843925cb
alexlewis9 <150391000+alexlewis9@users.noreply.github.com>
2024-11-09 21:05:47 -0500

    company logos

--- src/for-students-alumni/Alumni.tsx
+++ src/for-students-alumni/Alumni.tsx
@@ -1,27 +1,41 @@
-import { Center, Title } from "@mantine/core";
-// import "./Alumni.css";
+import { Center, Title, Grid, Image } from "@mantine/core";
+import "./Alumni.css";
+
+const companies = [
+  { src: "/logos/meta.svg", alt: "Meta" },
+  { src: "/logos/google.svg", alt: "Google" },
+  { src: "/logos/stripe.svg", alt: "Stripe" },
+  { src: "/logos/microsoft.svg", alt: "Microsoft" },
+  { src: "/logos/amazon.svg", alt: "Amazon" },
+  { src: "/logos/intuit.svg", alt: "Intuit" },
+  { src: "/logos/intel.svg", alt: "Intel" },
+  { src: "/logos/figma.svg", alt: "Figma" },
+  { src: "/logos/coursera.svg", alt: "Coursera" },
+  { src: "/logos/rbc.svg", alt: "RBC" },
+  { src: "/logos/citadel.svg", alt: "Citadel" },
+  { src: "/logos/square.svg", alt: "Square" },
+];
 
 function Alumni() {
   return (
-    <div
-      className="alumni"
-      style={{
-        borderRadius: "0px 0px 300px 300px",
-        backgroundColor: "#e7f3ff",
-        color: "#0078e8",
-      }}
-    >
+    <div className="alumni">
       <Center>
-        <Title
-          className="alumni-title"
-          size="3vw"
-          style={{
-            padding: "5vw",
-            fontSize: window.innerWidth <= 800 ? "20px" : "3vw",
-          }}
-        >
+        <Title className="alumni-title" size="3vw">
           Our Alumni have gone on to innovate at:
         </Title>
+      </Center>
+      <Center>
+        <Grid justify="center" align="center" w="60%">
+          {companies.map((company, index) => (
+            <Grid.Col key={index} span={3}>
+              <Image
+                src={company.src}
+                alt={company.alt}
+                className="company-logo"
+              />
+            </Grid.Col>
+          ))}
+        </Grid>
       </Center>
     </div>
   );
