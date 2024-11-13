ea316d3751780a4f9f7ee6dcb745caf8337a7a02
Saleh Yasin <saleh.yasin@mail.utoronto.ca>
2024-11-13 18:10:50 -0500

    Fixed Links
    Fixed links for UC Irvine, UC Boulder and UC Carleton. Additionally, fixed the body for Alabama and Boulder so that they display the right established year

--- src/app/chapters/page.tsx
+++ src/app/chapters/page.tsx
@@ -8,7 +8,7 @@ import {Grid} from "@mantine/core";
 const infoCAN = [
   {title: "University of Waterloo", description: 'UWaterloo was the first Blueprint chapter, founded in 2015 🍁', link:'https://uwblueprint.org/'},
   {title: "University of Toronto", description: 'The second Canadian chapter to be established, started in 2020 🌿', link: 'https://uoftblueprint.org/'},
-  {title: "Carleton University", description: "The third Blueprint chapter in Canada 🍂", link: "https://linktr.ee/cublueprint"},
+  {title: "Carleton University", description: "The third Blueprint chapter in Canada 🍂", link: "https://carletonblueprint.org/"},
 ]
 
 const rowCAN = infoCAN.map((info)=> (
@@ -17,11 +17,11 @@ const rowCAN = infoCAN.map((info)=> (
 
 //USA
 const infoUSA = [
-  {title: "UC Berkley", description: 'Blueprint was founded at UC Berkeley in Spring 2013.', link:'https://calblueprint.org/'},
+  {title: "UC Berkley", description: 'Blueprint was founded at UC Berkeley in Spring 2013 🏡', link:'https://calblueprint.org/'},
   {title: "UCLA", description: 'Blueprint at the University of California, Los Angeles was established in 2019 🚀', link: 'https://lablueprint.org/'},
-  {title: "UC Irvine", description: "Our second Southern California Blueprint chapter was established in 2020 ♥️", link: "https://uciblueprint.org/#/"},
-  {title: "University of Alabama", description: 'Another BP chapter that was started in the US ✨', link:'https://website.sdwight.workers.dev/'},
-  {title: "University of Colorado Boulder", description: 'Currently in the process of establishing in CU Boulder 💕', link: 'https://calblueprint.org/chapters'},
+  {title: "UC Irvine", description: "Our second Southern California Blueprint chapter was established in 2020 ♥️", link: "https://blueprintatuci.github.io/"},
+  {title: "University of Alabama", description: 'Blueprint at University of Alabama was established in 2024 ✨', link:'https://website.sdwight.workers.dev/'},
+  {title: "University of Colorado Boulder", description: 'Blueprint was founded at University of Colorado Boulder in 2020 💕', link: 'https://blueprintboulder.org/'},
 ];
 
 
