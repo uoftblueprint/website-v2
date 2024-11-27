9e3c451003ee9db131e69a7975076e319003bc44
Alex Lewis <150391000+alexlewis9@users.noreply.github.com>
2024-11-27 18:39:33 -0500

    Alexlewis/intl 17 for students page skeleton (#50)
    * forstudents page
    
    * change button no anchor
    
    * icons on role cards and sizing/spacing of role cards
    
    * faq chevron
    
    * remove css alumni, roles
    
    * rename to for-students
    
    * some role card css
    
    * fix process image width spacing
    
    * alumni text sizing
    
    * fix role cards text and icons to not get too small
    
    * fix apply now button text cut off on wide window
    
    * remove alumni css
    
    * adjust padding
    
    * fix chevron
    
    * company logos
    
    * adjust padding bottom
    
    * forstudents page
    
    * change button no anchor
    
    * icons on role cards and sizing/spacing of role cards
    
    * faq chevron
    
    * remove css alumni, roles
    
    * rename to for-students
    
    * some role card css
    
    * fix process image width spacing
    
    * alumni text sizing
    
    * fix role cards text and icons to not get too small
    
    * fix apply now button text cut off on wide window
    
    * remove alumni css
    
    * adjust padding
    
    * fix chevron
    
    * company logos
    
    * adjust padding bottom
    
    * mobile view +
    
    * fixes
    
    * alumni title font sizing
    
    * alumni title font size
    
    ---------
    
    Co-authored-by: Jordan Janakievski <jajanak10@gmail.com>

---
+++ src/for-students-alumni/Alumni.tsx
@@ -0,0 +1,44 @@
+import { Center, Title, Grid, Image } from "@mantine/core";
+import "./Alumni.css";
+
+const companies = [
+  { src: "/logos/meta.svg", alt: "Meta" },
+  { src: "/logos/google.svg", alt: "Google" },
+  { src: "/logos/stripe.svg", alt: "Stripe" },
+  { src: "/logos/microsoft.svg", alt: "Microsoft" },
+  { src: "/logos/amazon.svg", alt: "Amazon" },
+  { src: "/logos/intuit.svg", alt: "Intuit" },
+  { src: "/logos/intel.svg", alt: "Intel" },
+  { src: "/logos/figma.svg", alt: "Figma" },
+  { src: "/logos/coursera.svg", alt: "Coursera" },
+  { src: "/logos/rbc.svg", alt: "RBC" },
+  { src: "/logos/citadel.svg", alt: "Citadel" },
+  { src: "/logos/square.svg", alt: "Square" },
+];
+
+function Alumni() {
+  return (
+    <div className="alumni">
+      <Center>
+        <Title className="alumni-title" size="3vw">
+          Our Alumni have gone on to innovate at:
+        </Title>
+      </Center>
+      <Center>
+        <Grid justify="center" align="center" w="60%">
+          {companies.map((company, index) => (
+            <Grid.Col key={index} span={3}>
+              <Image
+                src={company.src}
+                alt={company.alt}
+                className="company-logo"
+              />
+            </Grid.Col>
+          ))}
+        </Grid>
+      </Center>
+    </div>
+  );
+}
+
+export default Alumni;
---
+++ src/for-students-roles/Roles.tsx
@@ -0,0 +1,49 @@
+"use client";
+import RoleCard from "../RoleCard/RoleCard";
+import { Center, Divider, Grid, Title } from "@mantine/core";
+import "./Roles.css";
+
+const info = [
+  { role: "Software Developer", link: undefined },
+  { role: "Software Developer", link: undefined },
+  { role: "Software Developer", link: undefined },
+];
+
+const row = info.map((info) => (
+  <Grid.Col
+    span={{ xs: 12, sm: 6, md: 4, lg: 3 }}
+    key={info.role}
+    className={"card"}
+  >
+    {" "}
+    <RoleCard role={info.role} link={info.link}></RoleCard>
+  </Grid.Col>
+));
+
+export default function Roles() {
+  return (
+    <div>
+      <Center>
+        <Title
+          className="open-roles-title"
+          size="4vw"
+          style={{
+            color: "#0078e8",
+            padding: "4vw 0vw 2vw 0vw",
+          }}
+        >
+          Open Roles
+        </Title>
+      </Center>
+      <Divider w="30%" color="#e7f3ff" size={"1px"} mx="auto" />
+      <Grid
+        justify="space-evenly"
+        align={"center"}
+        className={"grid"}
+        style={{ padding: "2vw 4vw 4vw 4vw" }}
+      >
+        {row}
+      </Grid>
+    </div>
+  );
+}
